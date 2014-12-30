b5e4fea523ed388df1f2d2a745f0cdc348734138
Kevin Finelli <k.finelli@gmail.com>
2014-12-30 15:16:02 +1100

    fix backspace after completion problem

--- lib/completion.zsh
+++ lib/completion.zsh
@@ -11,14 +11,14 @@ WORDCHARS=''
 zmodload -i zsh/complist
 
 ## case-insensitive (all),partial-word and then substring completion
-if [ "x$CASE_SENSITIVE" = "xtrue" ]; then
-  zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-  unset CASE_SENSITIVE
-else
-  zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-fi
-
-zstyle ':completion:*' list-colors ''
+##if [ "x$CASE_SENSITIVE" = "xtrue" ]; then
+##  zstyle ':completion:*' matcher-list 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+##  unset CASE_SENSITIVE
+##else
+##  zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+##fi
+##
+##zstyle ':completion:*' list-colors ''
 
 # should this be in keybindings?
 bindkey -M menuselect '^o' accept-and-infer-next-history
